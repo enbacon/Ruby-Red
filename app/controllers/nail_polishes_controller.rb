b00d015d2cf05157bd15a362facf76bab166dce9
enbacon <bacon.erin@gmail.com>
2019-09-07 18:10:40 -0400

    Change authorization on controller
    Changed controller to an OpenReadController

--- app/controllers/nail_polishes_controller.rb
+++ app/controllers/nail_polishes_controller.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
+#
 
-class NailPolishesController < ApplicationController
-  before_action :set_nail_polish, only: %i[show update destroy]
+# frozen_string_literal: true
+
+class NailPolishesController < OpenReadController
+  before_action :set_nail_polish, only: %i[update destroy]
 
   # GET /nail_polishes
+  # GET /nail_polishes.json
   def index
     @nail_polishes = NailPolish.all
 
@@ -11,22 +15,25 @@ class NailPolishesController < ApplicationController
   end
 
   # GET /nail_polishes/1
+  # GET /nail_polishes/1.json
   def show
-    render json: @nail_polish
+    render json: NailPolish.find(params[:id])
   end
 
   # POST /nail_polishes
+  # POST /nail_polishes.json
   def create
-    @nail_polish = NailPolish.new(nail_polish_params)
+    @nail_polish = current_user.nail_polishes.build(nail_polish_params)
 
     if @nail_polish.save
-      render json: @nail_polish, status: :created, location: @nail_polish
+      render json: @nail_polish, status: :created
     else
       render json: @nail_polish.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /nail_polishes/1
+  # PATCH/PUT /nail_polishes/1.json
   def update
     if @nail_polish.update(nail_polish_params)
       render json: @nail_polish
@@ -36,19 +43,82 @@ class NailPolishesController < ApplicationController
   end
 
   # DELETE /nail_polishes/1
+  # DELETE /nail_polishes/1.json
   def destroy
     @nail_polish.destroy
+
+    head :no_content
   end
 
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
   def set_nail_polish
-    @nail_polish = NailPolish.find(params[:id])
+    @nail_polish = current_user.nail_polishes.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def nail_polish_params
     params.require(:nail_polish).permit(:brand, :color_name, :color)
   end
+
+  private :set_nail_polish, :nail_polish_params
 end
+
+
+
+# #
+# #
+# #
+# #
+# #
+# #
+#
+# class NailPolishesController < OpenReadController
+#   before_action :set_nail_polish, only: %i[update destroy]
+#
+#   # GET /nail_polishes
+#   def index
+#     @nail_polishes = NailPolish.all
+#
+#     render json: @nail_polishes
+#   end
+#
+#   # GET /nail_polishes/1
+#   def show
+#     render json: @nail_polish
+#   end
+#
+#   # POST /nail_polishes
+#   def create
+#     @nail_polish = current_user.nail_polish.build(nail_polish_params)
+#
+#     if @nail_polish.save
+#       render json: @nail_polish, status: :created, location: @nail_polish
+#     else
+#       render json: @nail_polish.errors, status: :unprocessable_entity
+#     end
+#   end
+#
+#   # PATCH/PUT /nail_polishes/1
+#   def update
+#     if @nail_polish.update(nail_polish_params)
+#       render json: @nail_polish
+#     else
+#       render json: @nail_polish.errors, status: :unprocessable_entity
+#     end
+#   end
+#
+#   # DELETE /nail_polishes/1
+#   def destroy
+#     @nail_polish.destroy
+#   end
+#
+#   private
+#
+#   # Use callbacks to share common setup or constraints between actions.
+#   def set_nail_polish
+#     @nail_polish = NailPolish.find(params[:id])
+#   end
+#
+#   # Only allow a trusted parameter "white list" through.
+#   def nail_polish_params
+#     params.require(:nail_polish).permit(:brand, :color_name, :color)
+#   end
+# end
--- curl-scripts/nail-polishes/delete-polish.sh
+++ curl-scripts/nail-polishes/delete-polish.sh
@@ -3,5 +3,7 @@
 curl "http://localhost:4741/nail_polishes/${ID}" \
   --include \
   --request DELETE \
+  --header "Authorization: Token token=${TOKEN}" \
+
 
 echo
